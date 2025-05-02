89146d5e618a0968ec444631d56631801097e1a7
Rodrigo Motti de Santana <113243243+rmotti@users.noreply.github.com>
2025-05-02 16:25:51 +0000

    feat: aula 04

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import db from './database/configdb.js';
+import User from './models/User.js';
 
 dotenv.config();
+db.connect();
+
+
 const app = express();
 
 
