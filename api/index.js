3ebbd2aa086be12dc8f12fd0dc665bf00c7ffa04
Rodrigo Motti de Santana <113243243+rmotti@users.noreply.github.com>
2025-05-18 23:10:37 -0300

    feat: test

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import db from './database/configdb.js';
 import userRoute from './routes/user.route.js';
 import exampleRoute from './routes/example.route.js';
@@ -12,6 +13,19 @@ const app = express();
 
 app.use(express.json());
 
+// Configuração de CORS
+const corsOptions = {
+  origin: [
+    'http://localhost:3000', // Permite requisições locais durante o desenvolvimento
+    'https://finsync-frontend.vercel.app' // Permite requisições do frontend na Vercel
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
+
 // Rotas públicas
 app.use("/users", userRoute);
 
@@ -19,11 +33,12 @@ app.use("/users", userRoute);
 app.use("/transactions", transactionRoute);
 app.use("/secureExampleRoute", exampleRoute);
 
+// Rota principal
 app.get('/', (req, res) => {
-    res.send({message:'API Started'});
+  res.send({ message: 'API Started' });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`API server is running on port http://localhost:${PORT}/`);
 });
