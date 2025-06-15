926f0c034dbec88673d8ac8f185370d316d8bf8a
Gustavo Ferreira Cabral <gugacabral03@gmail.com>
2025-06-15 13:31:02 -0300

    fix model

--- api/models/Transaction.js
+++ api/models/Transaction.js
@@ -4,32 +4,32 @@ const transactionSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   amount: {
     type: Number,
-    required: true
+    required: true,
   },
   type: {
     type: String,
-    enum: ['income', 'expense'],
-    required: true
+    enum: ['Despesa', 'Receita'],
+    required: true,
   },
   category: {
     type: String,
-    required: true
+    required: true,
+    trim: true,
   },
   date: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: true
-  }
+    required: true,
+  },
 });
 
-    const Transaction = mongoose.model('Transaction', transactionSchema);
-
-    export default Transaction;
+const Transaction = mongoose.model('Transaction', transactionSchema);
+export default Transaction;
